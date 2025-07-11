8e24d995fe05313c379b2c8b4981368a3bb28aca
CoderistaJong <coderistajong@gmail.com>
2025-07-11 22:44:39 +0900

    feat: 그림 추가

--- main.py
+++ main.py
@@ -1,7 +1,94 @@
 import tkinter as tk
 import customtkinter as ctk
 import turtle
+import colorsys
+import math
 import threading    # GUI 2개를 동시에 하기 위함
+
+
+# 1번. 꽃
+def draw_flower():
+    t = turtle.Turtle()
+    t.speed(0)
+    t.hideturtle()
+    hue = 0
+    
+    for _ in range(15):
+        for j in range(18):
+            c = colorsys.hsv_to_rgb(hue, 1, 1)
+            t.color(c)
+            hue += 0.005
+            t.right(90)
+            t.circle(150 - j * 6, 90)
+            t.left(90)
+            t.circle(150 - j * 6, 90)
+            t.right(180)
+        t.circle(40, 24)
+
+
+# 2번. 만다라
+def draw_mandara():
+    t = turtle.Turtle()
+    t.speed(0)
+    t.hideturtle()
+    hue = 0
+    n = 36
+
+    for i in range(240):
+        c = colorsys.hsv_to_rgb(hue, 1, 1)
+        t.pencolor(c)
+        t.width(i / 100 + 1)
+
+        t.forward(i)
+        t.left(59)
+        hue += 1 / n
+
+
+# 3번. 스파이럴 플라워
+def draw_spiral_flower():
+    t = turtle.Turtle()
+    t.speed(0)
+    t.hideturtle()
+    hue = 0
+
+    for _ in range(60):
+        c = colorsys.hsv_to_rgb(hue, 1, 1)
+        t.color(c)
+
+        t.circle(100)
+        t.left(6)
+        hue += 1 / 60
+
+
+# 4번. 장미 곡선
+def draw_rose_curve():
+    t = turtle.Turtle()
+    t.speed(0)
+    t.width(2)
+    t.hideturtle()
+    hue = 0
+
+    for theta in range(210):
+        c = colorsys.hsv_to_rgb(hue, 1, 1)
+        t.color(c)
+        angle = math.radians(theta)
+        r = 200 * math.sin(13 * angle)
+        x = r * math.cos(angle)
+        y = r * math.sin(angle)
+
+        t.goto(x, y)
+
+        if theta == 0:
+            t.penup()
+        else:
+            t.pendown()
+        hue += 1 / 180
+
+
+
+
+# 선택된 그림 전역변수
+func = draw_mandara
 
 
 # turtle을 이용한 그리기 함수
@@ -13,14 +100,7 @@ def turtle_draw():
         screen.clearscreen
         screen.bgcolor("black")
 
-        t = turtle.Turtle()
-        t.speed(0)
-        t.color("cyan")
-        t.hideturtle()
-
-        for _ in range(5):
-            t.forward(100)
-            t.right(90)
+        func()
 
         screen.exitonclick()
         screen.bye()
@@ -31,7 +111,8 @@ def turtle_draw():
 
 # 버튼 클릭 시 turtle 실행 함수
 def on_click():
-    turtle_thread = threading.Thread(target=turtle_draw, daemon= True).start()
+    turtle_thread = threading.Thread(target=turtle_draw, daemon= True)
+    turtle_thread.start()
 
 
 # 테마 설정
@@ -41,10 +122,32 @@ ctk.set_default_color_theme("blue")
 # 창 설정
 root = ctk.CTk()
 root.title("Turtle Draw")
-root.geometry("1000x600")
+root.geometry("1000x600+200+100")
 
-button = ctk.CTkButton(root, text="실행", command=turtle_draw)
-button.pack(pady=100)
+
+
+# grid
+root.columnconfigure(0, weight=1)
+root.rowconfigure(0, weight=8, minsize=700)
+root.rowconfigure(1, weight=1, minsize=100)
+
+# 레이아웃
+top_frame = ctk.CTkFrame(root)
+top_frame.grid(row=0, column=0, sticky="nsew")
+
+bottom_frame = ctk.CTkFrame(root, fg_color="#444444")
+bottom_frame.grid(row=1, column=0, sticky="nsew")
+
+
+# top_frame 레이아웃
+
+
+
+
+# bottom_frame 레이아웃
+btn_run = ctk.CTkButton(bottom_frame, text="실행", command=turtle_draw, width=150, height=40, font=("Arial", 18))
+btn_run.pack(pady=20)
+
 
 
 root.mainloop()
